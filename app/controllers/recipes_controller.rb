3d15d04cafe449ce74217c1d15775be59d781fb9
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-14 21:49:50 -0400

    Adds all recipes controller methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,6 +11,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end 
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end 
+
   def create
     @recipe = Recipe.new(recipe_params)
 
@@ -19,6 +23,23 @@ class RecipesController < ApplicationController
     else
       render 'new'
     end 
+  end 
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end 
+  end 
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipes_path
   end 
 
   private
