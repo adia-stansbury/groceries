0c244b6288e682705dc4791764b3435f13a5cce8
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-14 21:39:16 -0400

    Removes recipe_ing#new

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,12 +1,7 @@
 class RecipeIngredientsController < ApplicationController
-  def new 
-    @recipe_ingredient = RecipeIngredient.new
-    @ingredients = Ingredient.all.order(:name)
-    @recipes = Recipe.all.order(:name)
-  end 
-
   def create
     @recipe = Recipe.find(params[:recipe_id])
+    @ingredients = Ingredient.all.order(:name)
     @recipe_ingredient = @recipe.recipe_ingredients.create(recipe_ingredient_params)
     redirect_to recipe_path(@recipe)
   end 
