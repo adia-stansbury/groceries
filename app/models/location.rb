de83e8dc799f69607654162d38d85740b9ad2be2
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-17 21:25:56 -0400

    Adds dependent destroy for all models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  has_many :ingredients
+  has_many :ingredients, dependent: :destroy
   has_many :recipe_ingredients, through: :ingredients
 
   validates :name, uniqueness: true
--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,5 +1,5 @@
 class Nutrient < ActiveRecord::Base
-  has_many :ingredient_nutrients
+  has_many :ingredient_nutrients, dependent: :destroy
   has_many :ingredients, through: :ingredient_nutrients
 
   validates :name, uniqueness: true, presence: true
