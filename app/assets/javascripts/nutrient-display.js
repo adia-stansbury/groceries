3023f03fa16be22ed9c80b687d9ef6e44c9ff5ed
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-24 11:29:22 -0400

    Collapses group

--- app/assets/javascripts/nutrient-display.js
+++ app/assets/javascripts/nutrient-display.js
@@ -2,9 +2,14 @@ $( document ).ready(function() {
   var groups = ["amino-acids", "lipids", "minerals", "proximates", "vitamins"];
   $.each(groups, function( index, group ) {
     $( ".plus-sign-" + group).click(function() {
+      $( ".plus-sign-" + group).hide();
       $( ".minus-sign-" + group).show();
       $( ".nutrient-info-" + group).show();
-      $( ".plus-sign-" + group).hide();
+    });
+    $( ".minus-sign-" + group).click(function() {
+      $( ".minus-sign-" + group).hide();
+      $( ".plus-sign-" + group).show();
+      $( ".nutrient-info-" + group).hide();
     });
   });
 });
