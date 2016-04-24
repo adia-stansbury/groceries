e4207526b6c982087295b946a13c9767d50193a2
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-24 09:27:33 -0400

    Changes class to id

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require('bootstrap')
+//= require nutrient-display
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
---
+++ app/assets/javascripts/nutrient-display.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+  $( "#plus-sign").click(function() {
+    $( "#minus-sign").show();
+    $( "#nutrient-info").show();
+    $( "#plus-sign").hide();
+  });
+});
