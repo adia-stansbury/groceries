9f5b3ee18540beb9635e7a8a68c7a8b407506065
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-24 10:10:56 -0400

    WIP: writing js fx to loop through array of groups
    Need to indicate class specific to group to only select specific group to
    colllapse

--- app/assets/javascripts/nutrient-display.js
+++ app/assets/javascripts/nutrient-display.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
-  $( "#plus-sign").click(function() {
-    $( "#minus-sign").show();
-    $( "#nutrient-info").show();
-    $( "#plus-sign").hide();
-  });
+  var groups = ["amino-acids", "lipids", "minerals", "proximates", "vitamins"];
+  function toggleGroupDisplay(group) {
+    $( ".plus-sign-" + group).click(function() {
+      $( ".minus-sign-" + group).show();
+      $( ".nutrient-info-" + group).show();
+      $( ".plus-sign-" + group).hide();
+    });
+  }
+  $( ".plus-sign" ).click(function(groups.forEach(toggleGroupDisplay)));
 });
