c221d6eedf27e95453d0530606b591656ee8f8f8
Adia Stansbury <adia.stansbury@gmail.com>
2016-04-24 20:01:01 -0400

    Adds after save method for ing to create ing_nut record

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,28 @@ class Ingredient < ActiveRecord::Base
   belongs_to :location
 
   validates :name, uniqueness: true, presence: true
+  validates :location_id, presence: true
+
+  after_save :create_ingredient_nutrient_record
+
+  private
+
+  def create_ingredient_nutrient_record
+    if self.ndbno.present?
+      ndb_usda_api_key = ENV['NDB_USDA_API_KEY']
+      ndbno = self.ndbno
+      HTTParty.get(
+        "http://api.nal.usda.gov/ndb/reports/?ndbno=#{ndbno}&type=f&format=json&api_key=#{ndb_usda_api_key}"
+      )['report']['food']['nutrients'].each do |nutrient|
+        ingredient_nutrition = IngredientNutrient.new
+        ingredient_nutrition.ingredient_id = self.id
+        ingredient_nutrition.nutrient_id = Nutrient.where(
+          name: nutrient['name']
+        ).first.id
+        ingredient_nutrition.value = nutrient['value']
+        ingredient_nutrition.unit = nutrient['unit']
+        ingredient_nutrition.save
+      end 
+    end 
+  end 
 end 
