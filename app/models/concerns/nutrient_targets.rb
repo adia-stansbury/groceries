e6fc32f3fc4bc5952e3b1c232498126ad5b4914c
Adia Stansbury <adia.stansbury@gmail.com>
2016-06-17 10:56:54 -0400

    Adds method to create target hash per nutrient

--- app/models/concerns/nutrient_targets.rb
+++ app/models/concerns/nutrient_targets.rb
@@ -3,11 +3,23 @@ module NutrientTargets
 
   module ClassMethods
     def nutrient_target_hash
-      nutrient_targets = {
-        Iron: {
-          unit: 'mg/d',
-          Mick: 8,
-          Adia: 15
+      {
+        create_goal_hash('Iron, Fe', 'mg', 8, 18)
+      }
+    end 
+
+    private
+
+    def create_goal_hash(nutrient_name, unit, mick_daily_goal, adia_daily_goal)
+      nutrient_name: {
+        unit: unit,
+        day: {
+          Mick: mick_daily_goal,
+          Adia: adia_daily_goal
+        },
+        week: {
+          Mick: mick_daily_goal * 7,
+          Adia: adia_daily_goal * 7
         }
       }
     end 
