10e8d8c8106088675e73288890cf57b12e6e7a47
Adia Stansbury <adia.stansbury@gmail.com>
2016-06-17 07:33:47 -0400

    Adds iron target

--- app/models/concerns/nutrient_targets.rb
+++ app/models/concerns/nutrient_targets.rb
@@ -2,7 +2,14 @@ module NutrientTargets
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def nutrient_target(nutrient, consumer)
+    def nutrient_target_hash
+      nutrient_targets = {
+        Iron: {
+          unit: 'mg/d',
+          Mick: 8,
+          Adia: 15
+        }
+      }
     end 
   end 
 end 
