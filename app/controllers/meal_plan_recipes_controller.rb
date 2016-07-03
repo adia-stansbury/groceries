be50834d4bfc1dd8292f311d99594c432641844e
Adia Stansbury <adia.stansbury@gmail.com>
2016-07-03 18:55:33 -0400

    Saves meal_plan_recipe for one record

--- app/controllers/meal_plan_recipes_controller.rb
+++ app/controllers/meal_plan_recipes_controller.rb
@@ -1,24 +1,15 @@
 class MealPlanRecipesController < ApplicationController
-  def new 
-  end 
-
   def create
-    @meal_plan_recipe = MealPlanRecipe.new(meal_plan_recipe_params)
-    #first_or_initialize MealPlan
-    mealplan = MealPlan.create(consumer_id: params[:consumer_id])
-    @meal_plan_recipe.meal_plan_id = mealplan.id
-    
-    if @meal_plan_recipe.save
-      redirect_to '/'
-    else
-      render 'new'
-    end 
+    @meal_plan = MealPlan.find(params[:meal_plan_id])
+    @meal_plan_recipe = @meal_plan.meal_plan_recipes.create(meal_plan_recipe_params)
+    @meal_plan_recipe.recipe_id = params[:recipe_id].first.to_i
+    @meal_plan_recipe.save
+    redirect_to meal_plan_path(@meal_plan)
   end 
 
   private
   def meal_plan_recipe_params
-    params.require(:meal_plan_recipe).permit(:recipe_id)
+    params.require(:meal_plan_recipe).permit(:number_of_recipes)
   end 
 end 
-
 
