9d3d846a628ceac07893e5584a5e1b8645305f2e
Adia Stansbury <adia-stansbury@users.noreply.github.com>
2016-07-09 18:44:08 -0400

    Displays num of servings (#45)

--- app/controllers/meal_plan_recipes_controller.rb
+++ app/controllers/meal_plan_recipes_controller.rb
@@ -1,15 +1,12 @@
 class MealPlanRecipesController < ApplicationController
   def create
     @meal_plan = MealPlan.find(params[:meal_plan_id])
-    @meal_plan_recipe = @meal_plan.meal_plan_recipes.create(meal_plan_recipe_params)
+    @meal_plan_recipe = @meal_plan.meal_plan_recipes.create
     @meal_plan_recipe.recipe_id = params[:recipe_id].first.to_i
     @meal_plan_recipe.save
     redirect_to meal_plan_path(@meal_plan)
   end 
 
   private
-  def meal_plan_recipe_params
-    params.require(:meal_plan_recipe).permit(:number_of_recipes)
-  end 
 end 
 
