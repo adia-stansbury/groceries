7a50c41b601f3f8cecf96bd2c4868909bed16ad2
Adia Stansbury <adia-stansbury@users.noreply.github.com>
2016-08-07 13:11:07 -0400

    Display intakes in percents of rda (#55)
    * Adds empty partials and adds to show htmls to render them
    
    * Adds space btw amt and unit for nutrient intake
    
    * Displays percent of daily RDA
    
    * Displays percent of weekly RDA

--- app/models/concerns/nutrient_targets.rb
+++ app/models/concerns/nutrient_targets.rb
@@ -8,14 +8,14 @@ module NutrientTargets
     def daily_rda_hash
       {
         'Calcium, Ca': create_goal_hash('mg', 1000, 1000),
-        'Copper, Cu': create_goal_hash('ug', 900, 900),
+        'Copper, Cu': create_goal_hash('mg', 0.9, 0.9),
         'Iron, Fe': create_goal_hash('mg', 8, 18),
         'Magnesium, Mg': create_goal_hash('mg', 420, 310),
         'Manganese, Mn': create_goal_hash('mg', 2.3, 1.8),
         'Phosphorus, P': create_goal_hash('mg', 700, 700),
-        'Potassium, K': create_goal_hash('g', 4.7, 4.7),
+        'Potassium, K': create_goal_hash('mg', 4700, 4700),
         'Selenium, Se': create_goal_hash('ug', 55, 55),
-        'Sodium, Na': create_goal_hash('g', 1.5, 1.5),
+        'Sodium, Na': create_goal_hash('mg', 1500, 1500),
         'Zinc, Zn': create_goal_hash('mg', 11, 8),
         'Fiber, total dietary': create_goal_hash('g', 38, 25),
         'Protein': create_goal_hash(
@@ -23,93 +23,97 @@ module NutrientTargets
           adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.8),
           adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.8)
         ),
-        'Alanine': create_goal_hash('no RDA', '', ''),
-        'Arginine': create_goal_hash('no RDA', '', ''),
-        'Aspartic acid': create_goal_hash('no RDA', '', ''),
+        'Alanine': create_goal_hash('no RDA', 0, 0),
+        'Arginine': create_goal_hash('no RDA', 0, 0),
+        'Aspartic acid': create_goal_hash('no RDA', 0, 0),
         'Cystine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 19),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 19)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.019),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.019)
         ),
-        'Glutamic acid': create_goal_hash('no RDA', '', ''),
-        'Glycine': create_goal_hash('no RDA', '', ''),
+        'Glutamic acid': create_goal_hash('no RDA', 0, 0),
+        'Glycine': create_goal_hash('no RDA', 0, 0),
         'Histidine': create_goal_hash(
-          'mg', 
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 14), 
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 14)
+          'g', 
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.014), 
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.014)
         ),
         'Isoleucine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 19),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 19)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.019),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.019)
         ),
         'Leucine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 42),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 42)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.042),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.042)
         ),
         'Lysine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 38),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 38)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.038),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.038)
         ),
         'Methionine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 19),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 19)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.019),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.019)
         ),
         'Phenylalanine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 33),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 33)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.033),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.033)
         ),
-        'Proline': create_goal_hash('no RDA', '', ''),
-        'Serine': create_goal_hash('no RDA', '', ''),
+        'Proline': create_goal_hash('no RDA', 0, 0),
+        'Serine': create_goal_hash('no RDA', 0, 0),
         'Threonine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 20),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 20)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.020),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.020)
         ),
         'Tryptophan': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 5),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 5)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.005),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.005)
         ),
         'Tyrosine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 33),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 33)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.033),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.033)
         ),
         'Valine': create_goal_hash(
-          'mg',
-          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 24),
-          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 24)
+          'g',
+          adjust_RDA_for_weight(MICK_WEIGHT_LBS, 0.024),
+          adjust_RDA_for_weight(ADIA_WEIGHT_LBS, 0.024)
         ),
-        'Betaine': create_goal_hash('no RDA', '', ''),
-        'Carotene, alpha': create_goal_hash('no RDA', '', ''),
-        'Carotene, beta': create_goal_hash('no RDA', '', ''),
+        'Betaine': create_goal_hash('no RDA', 0, 0),
+        'Carotene, alpha': create_goal_hash('no RDA', 0, 0),
+        'Carotene, beta': create_goal_hash('no RDA', 0, 0),
         'Choline, total': create_goal_hash('mg', 550, 425), 
         'Folate, DFE': create_goal_hash('ug', 400, 400),
         'Folate, food': create_goal_hash('ug', 400, 400),
         'Folate, total': create_goal_hash('ug', 400, 400),
-        'Folic acid': create_goal_hash('no RDA', '', ''),
-        'Lutein + zeaxanthin': create_goal_hash('no RDA', '', ''),
-        'Lycopene': create_goal_hash('no RDA', '', ''),
+        'Folic acid': create_goal_hash('no RDA', 0, 0),
+        'Lutein + zeaxanthin': create_goal_hash('no RDA', 0, 0),
+        'Lycopene': create_goal_hash('no RDA', 0, 0),
         'Niacin': create_goal_hash('mg', 14, 14), 
         'Pantothenic acid': create_goal_hash('mg', 5, 5), 
-        'Retinol': create_goal_hash('no RDA', '', ''),
+        'Retinol': create_goal_hash('no RDA', 0, 0),
         'Riboflavin': create_goal_hash('mg', 1.1, 1.1),
         'Thiamin': create_goal_hash('mg', 1.2, 1.1),
-        'Vitamin A, IU': create_goal_hash('no RDA', '', ''),
+        'Vitamin A, IU': create_goal_hash('no RDA', 0, 0),
         'Vitamin A, RAE': create_goal_hash('ug', 900, 700),
         'Vitamin B-12': create_goal_hash('ug', 2.4, 2.4), 
         'Vitamin B-6': create_goal_hash('mg', 1.3, 1.3), 
         'Vitamin C, total ascorbic acid': create_goal_hash('mg', 90, 75),
         'Vitamin D': create_goal_hash('ug', 5, 5),
-        'Vitamin D (D2 + D3)': create_goal_hash('no RDA', '', ''),
+        'Vitamin D (D2 + D3)': create_goal_hash('no RDA', 0, 0),
         'Vitamin E (alpha-tocopherol)': create_goal_hash('mg', 15, 15),
         'Vitamin K (phylloquinone)': create_goal_hash('ug', 120, 90) 
       }
+    end 
+
+    def percent_rda(intake, rda)
+      (intake.to_i.to_f/rda * 100).round(2)
     end 
 
     private
