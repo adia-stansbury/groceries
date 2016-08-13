c60547b3f55a39fb98a644dd49e78f7cf1b8dc9b
Adia Stansbury <adia-stansbury@users.noreply.github.com>
2016-08-12 22:15:10 -0400

    Creates meal plans from calendar (#61)

--- app/controllers/google_calendar_generated_meal_plans_controller.rb
+++ app/controllers/google_calendar_generated_meal_plans_controller.rb
@@ -56,63 +56,81 @@ class GoogleCalendarGeneratedMealPlansController < ApplicationController
     adia_meal_plan = MealPlan.create(consumer_id: 1)
     mick_meal_plan = MealPlan.create(consumer_id: 4)
 
-    lunches = get_calendar_events(ENV['LUNCH_CALENDAR_ID'])
-    dinners = get_calendar_events(ENV['DINNER_CALENDAR_ID'])
-
-    lunch_calendar_recipes = []
-    lunches.items.each do |recipe|
-      recipe_id = Recipe.where(name: recipe.summary).first.id
-      lunch_calendar_recipes << { meal_plan_id: adia_meal_plan.id, recipe_id: recipe_id, number_of_recipes: 1}
+    def get_recipe_names_from_calendar(calendar_id)
+      recipe_names = []
+      get_calendar_events(calendar_id).items.each do | event |
+        recipe_names << event.summary
+      end 
+      recipe_names
     end 
 
-    template_recipes = [
+    def get_hash_of_meal_plan_recipe_info(calendar_id)
+      meal_plan_recipe_info = []
+      recipe_ids = []
+      recipe_names = get_recipe_names_from_calendar(calendar_id)
+      recipe_names.each do |recipe_name|
+        recipe_ids << Recipe.where(name: recipe_name).first.id
+      end 
+      recipe_ids.each do |recipe_id|
+      #Recipe.where({name: recipe_names}).pluck(:id).each do |recipe_id|
+        #I had to get rid of this optimization b/c it was calling unique on
+        #recipe names
+        meal_plan_recipe_info << { 
+          recipe_id: recipe_id, 
+          number_of_recipes: 1
+        }
+      end 
+      meal_plan_recipe_info
+    end 
+
+    adia_template_recipes = [
       { # yogurts
         meal_plan_id: adia_meal_plan.id,
         recipe_id: 7, 
         number_of_recipes: 1
-      },
-      { 
-        meal_plan_id: mick_meal_plan.id,
+      }
+    ]
+
+    mick_template_recipes = [
+      { #yogurts 
         recipe_id: 13, 
         number_of_recipes: 1
       },
       {
         # 3 apples
-        meal_plan_id: mick_meal_plan.id,
         recipe_id: 44, 
         number_of_recipes: 1
       },
       {
-        meal_plan_id: mick_meal_plan.id,
         recipe_id: 44, 
         number_of_recipes: 1
       },
       {
-        meal_plan_id: mick_meal_plan.id,
         recipe_id: 44, 
         number_of_recipes: 1
       },
       {
         # 3 carbonated waters
-        meal_plan_id: mick_meal_plan.id,
         recipe_id: 21, 
         number_of_recipes: 1
       },
       {
-        meal_plan_id: mick_meal_plan.id,
         recipe_id: 21, 
         number_of_recipes: 1
       },
       {
-        meal_plan_id: mick_meal_plan.id,
         recipe_id: 21, 
         number_of_recipes: 1
       }
     ]
 
-    records_to_create = lunch_calendar_recipes + template_recipes 
+    adia_records_to_create = get_hash_of_meal_plan_recipe_info(ENV['ADIA_CALENDAR_ID']) + 
+      adia_template_recipes 
+    mick_records_to_create = get_hash_of_meal_plan_recipe_info(ENV['MICK_CALENDAR_ID']) + 
+      mick_template_recipes
 
-    MealPlanRecipe.create(records_to_create)
+    adia_meal_plan.meal_plan_recipes.create(adia_records_to_create)
+    mick_meal_plan.meal_plan_recipes.create(mick_records_to_create)
 
     redirect_to meal_plans_path
   end 
