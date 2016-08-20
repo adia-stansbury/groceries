fdac8c5467c648618fff69eafaf98155d6b5c109
Adia Stansbury <adia-stansbury@users.noreply.github.com>
2016-08-19 23:11:25 -0400

    Improve readability nutrient reports (#66)
    * Improves styling
    
    * Adds consumer/form to meal_plan/new
    
    * fixes table for ing/show
    
    * Adds page header to jumbotrons in recipe/show
    
    * Consolidates nutrient report into one table

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -18,6 +18,12 @@ class MealPlansController < ApplicationController
     @consumers = Consumer.all.order(:name)
     @recipes = Recipe.all.order(:name)
     @meal_plan = MealPlan.new
+    @consumer = Consumer.new
+    if params[:consumer].present?
+      @consumer = Consumer.new(consumer_params)
+      @consumer.save
+      render 'new'
+    end 
   end 
 
   def create
@@ -34,5 +40,9 @@ class MealPlansController < ApplicationController
   private
   def meal_plan_params
     params.require(:meal_plan).permit(:consumer_id)
+  end 
+
+  def consumer_params
+    params.require(:consumer).permit(:name)
   end 
 end 
