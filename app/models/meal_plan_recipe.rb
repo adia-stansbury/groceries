0148b815c3b8395512ae2d293ec49167c5b0566d
Adia Stansbury <adia.stansbury@gmail.com>
2017-01-15 22:43:14 -0500

    Adds hot chocolate to template recipes
    Milk should always be on grocery list

--- app/models/meal_plan_recipe.rb
+++ app/models/meal_plan_recipe.rb
@@ -4,42 +4,52 @@ class MealPlanRecipe < ActiveRecord::Base
 
   ADIA_TEMPLATE = [
     # yogurts
-    { 
-      recipe_id: 7, 
+    {
+      recipe_id: 7,
+      number_of_recipes: 1
+    },
+    # hot chocolate
+    {
+      recipe_id: 52,
       number_of_recipes: 1
     }
   ]
 
   MICK_TEMPLATE = [
-    #yogurts 
-    { 
-      recipe_id: 13, 
+    #yogurts
+    {
+      recipe_id: 13,
+      number_of_recipes: 1
+    },
+    # hot chocolate
+    {
+      recipe_id: 52,
       number_of_recipes: 1
     },
     # 3 apples
     {
-      recipe_id: 44, 
+      recipe_id: 44,
       number_of_recipes: 1
     },
     {
-      recipe_id: 44, 
+      recipe_id: 44,
       number_of_recipes: 1
     },
     {
-      recipe_id: 44, 
+      recipe_id: 44,
       number_of_recipes: 1
     },
     # 3 carbonated waters
     {
-      recipe_id: 21, 
+      recipe_id: 21,
       number_of_recipes: 1
     },
     {
-      recipe_id: 21, 
+      recipe_id: 21,
       number_of_recipes: 1
     },
     {
-      recipe_id: 21, 
+      recipe_id: 21,
       number_of_recipes: 1
     }
   ]
@@ -52,13 +62,13 @@ class MealPlanRecipe < ActiveRecord::Base
     recipe_ids = []
     recipe_names.each do |recipe_name|
       recipe_ids << Recipe.where(name: recipe_name).first.id
-    end 
+    end
     recipe_ids.each do |recipe_id|
-      meal_plan_recipe_rows << { 
-        recipe_id: recipe_id, 
+      meal_plan_recipe_rows << {
+        recipe_id: recipe_id,
         number_of_recipes: 1
       }
-    end 
+    end
     meal_plan_recipe_rows
-  end 
-end 
+  end
+end
