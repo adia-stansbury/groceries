8d75d5a3b2c060ee26efca16d3916f190fc29351
Adia Stansbury <adia.stansbury@gmail.com>
2018-01-29 21:55:54 -0500

    Displays un-formatted grocery list upon creation
    Allows pasting list to google keep and adding formatting on google keep

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,6 +1,6 @@
 class GroceryListsController < ApplicationController
   def new
-    list = GroceryList.new(params['meal_plan_ids'] * ',').create
-    EvernoteApi.new(list).create_note
+    @list = GroceryList.new(params['meal_plan_ids'] * ',').create
+    # EvernoteApi.new(@list).create_note
   end
 end
