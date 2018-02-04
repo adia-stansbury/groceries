42f9cc2704a66868ac075f554c6412c240642628
Adia Stansbury <adia.stansbury@gmail.com>
2018-02-04 07:34:06 -0500

    Adds created_at and updated_at timestamps to all tables
    Useful for debugging and inspecting/modifying newly imported data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115160213) do
+ActiveRecord::Schema.define(version: 20180204122521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.float "daily_rda"
     t.integer "consumer_id"
     t.integer "nutrient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["consumer_id"], name: "index_consumer_nutrients_on_consumer_id"
     t.index ["nutrient_id"], name: "index_consumer_nutrients_on_nutrient_id"
   end
@@ -31,6 +33,8 @@ ActiveRecord::Schema.define(version: 20180115160213) do
   create_table "consumers", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "weight_in_lbs"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "ingredient_nutrients", id: :serial, force: :cascade do |t|
@@ -38,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.string "unit", null: false
     t.integer "nutrient_id", null: false
     t.integer "ingredient_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["ingredient_id"], name: "index_ingredient_nutrients_on_ingredient_id"
     t.index ["nutrient_id"], name: "index_ingredient_nutrients_on_nutrient_id"
   end
@@ -50,6 +56,8 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.float "num_of_grams_in_measuring_amount"
     t.integer "unit_id"
     t.text "note"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["location_id"], name: "index_ingredients_on_location_id"
     t.index ["name"], name: "ingredients_name_key", unique: true
   end
@@ -58,6 +66,8 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.string "name", limit: 1000, null: false
     t.integer "ordering", null: false
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["name"], name: "locations_name_key", unique: true
     t.index ["ordering"], name: "locations_aisle_key", unique: true
   end
@@ -66,16 +76,21 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.integer "recipe_id", null: false
     t.integer "meal_plan_id", null: false
     t.date "date", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["meal_plan_id", "date", "recipe_id"], name: "index_meal_plan_recipes_on_meal_plan_id_and_date_and_recipe_id"
   end
 
   create_table "meal_plans", id: :serial, force: :cascade do |t|
     t.integer "consumer_id", null: false
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "nutrient_groups", id: :serial, force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "nutrients", id: :serial, force: :cascade do |t|
@@ -83,6 +98,8 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.integer "nutrient_group_id"
     t.float "upper_limit"
     t.integer "unit_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["unit_id"], name: "index_nutrients_on_unit_id"
   end
 
@@ -92,17 +109,23 @@ ActiveRecord::Schema.define(version: 20180115160213) do
     t.float "quantity"
     t.float "amount_in_grams", default: 0.0, null: false
     t.integer "unit_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "recipes", id: :serial, force: :cascade do |t|
     t.string "name", limit: 1000, null: false
     t.float "number_of_servings"
     t.text "note"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["name"], name: "recipes_name_key", unique: true
   end
 
   create_table "units", id: :serial, force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_foreign_key "consumer_nutrients", "consumers"
